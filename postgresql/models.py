71c860ac4f64834e273b8d6ea6de8d6412dc0fee
shawn365 <eogkr.kim@gmail.com>
2022-03-10 11:35:59 +0900

    modify/modeling

--- postgresql/models.py
+++ postgresql/models.py
@@ -22,17 +22,24 @@ class SnsInfo(Base):
 class Insight(Base):
     __tablename__ = 'insights'
 
-    id                     = Column(Integer, primary_key=True, index=True)
-    search_frequency       = Column(Integer)
-    visit_frequency        = Column(Integer)
-    like                   = Column(Integer)
-    comment                = Column(Integer)
-    male_follower          = Column(Integer)
-    female_follower        = Column(Integer)
-    bookmark               = Column(Integer)
-    created_at             = Column(DateTime, nullable=False, default=func.utc_timestamp())
-    influencer_post_id     = Column(ForeignKey('influencer_posts.id'))
-    influencer_id          = Column(ForeignKey('influencers.id'))
+    id                 = Column(Integer, primary_key=True, index=True)
+    search_frequency   = Column(Integer)
+    visit_frequency    = Column(Integer)
+    like               = Column(Integer)
+    comment            = Column(Integer)
+    male_follower      = Column(Integer)
+    female_follower    = Column(Integer)
+    bookmark           = Column(Integer)
+    exposure           = Column(Integer)
+    profile            = Column(Integer)
+    profile_visit      = Column(Integer)
+    website_click      = Column(Integer)
+    hashtag            = Column(Integer)
+    reaction           = Column(Integer)
+    home               = Column(Integer)
+    created_at         = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    influencer_post_id = Column(ForeignKey('influencer_posts.id'))
+    influencer_id      = Column(ForeignKey('influencers.id'))
 
     influencer = relationship('Influencer', primaryjoin='Insight.influencer_id == Influencer.id', backref='insights')
     influencer_post = relationship('InfluencerPost', primaryjoin='Insight.influencer_post_id == InfluencerPost.id',
@@ -41,11 +48,10 @@ class Insight(Base):
 class InfluencerPost(Base):
     __tablename__ = 'influencer_posts'
 
-    id                     = Column(Integer, primary_key=True, index=True)
-    tag                    = Column(String)
-    url                    = Column(Integer)
-    campaign_id            = Column(ForeignKey('campaigns.id'))
-    influencer_id          = Column(ForeignKey('influencers.id'))
+    id            = Column(String)
+    url           = Column(URLType)
+    campaign_id   = Column(ForeignKey('campaigns.id'))
+    influencer_id = Column(ForeignKey('influencers.id'))
 
     campaign = relationship('Campaign', primaryjoin='InfluencerPost.campaign_id == Campaign.id',
                                backref='influencer_posts')
@@ -73,33 +79,34 @@ class Company(Base):
 class Influencer(Base):
     __tablename__ = "influencers"
 
-    id              = Column(Integer, primary_key=True, index=True)
-    name            = Column(String)
-    profile_image   = Column(URLType)
-    created_at      = Column(DateTime, nullable=False, default=func.utc_timestamp())
-    updated_at      = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
-    company         = Column(Integer, ForeignKey('companies.id'))
+    id            = Column(Integer, primary_key=True, index=True)
+    name          = Column(String)
+    full_name     = Column(String)
+    profile_image = Column(URLType)
+    created_at    = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    updated_at    = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
+    company_id    = Column(Integer, ForeignKey('companies.id'))
 
-    company1 = relationship('Company', primaryjoin='Influencer.company == Company.id', backref='influencers')
+    company = relationship('Company', primaryjoin='Influencer.company_id == Company.id', backref='influencers')
 
 class Campaign(Base):
     __tablename__ = "campaigns"
 
-    id                 = Column(Integer, primary_key=True, index=True)
-    name               = Column(String)
-    tag                = Column(String)
-    description        = Column(String)
-    end_at             = Column(DateTime)
-    created_at         = Column(DateTime, nullable=False, default=func.utc_timestamp())
-    updated_at         = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
+    id          = Column(Integer, primary_key=True, index=True)
+    name        = Column(String)
+    tag         = Column(String)
+    description = Column(String)
+    end_at      = Column(DateTime)
+    created_at  = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    updated_at  = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
 
 class SocialNetworkService(Base):
     __tablename__ = "social_network_services"
 
-    id          = Column(Integer, primary_key=True, index=True)
-    name        = Column(String)
-    created_at  = Column(DateTime, nullable=False, default=func.utc_timestamp())
-    updated_at  = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
+    id         = Column(Integer, primary_key=True, index=True)
+    name       = Column(String)
+    created_at = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    updated_at = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
 
 
 
