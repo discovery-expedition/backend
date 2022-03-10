fe663388386c3a007eb1162f2d06238ee2f7ab29
shawn365 <eogkr.kim@gmail.com>
2022-03-10 17:10:57 +0900

    modify/modeling

--- postgresql/models.py
+++ postgresql/models.py
@@ -97,6 +97,7 @@ class Campaign(Base):
     tag         = Column(String)
     description = Column(String)
     end_at      = Column(DateTime)
+    image       = Column(URLType)
     created_at  = Column(DateTime, nullable=False, default=func.utc_timestamp())
     updated_at  = Column(DateTime, nullable=True, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
 
