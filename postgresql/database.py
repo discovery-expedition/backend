33a34c1103ab91b1bc776ab0ea62b52a013d0147
shawn365 <eogkr.kim@gmail.com>
2022-03-13 19:38:23 +0900

    create: main_page

--- postgresql/database.py
+++ postgresql/database.py
@@ -1,4 +1,5 @@
 import sys
+from click import echo
 sys.path.append('..')
 from sqlalchemy                 import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,7 +9,8 @@ from my_settings                import SQLALCHEMY_DATABASE_URL
 SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    echo = True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- postgresql/main.py
+++ postgresql/main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
-from database import engine
+from pip import main
 import models
+from fastapi  import FastAPI, Depends
+from database import engine
+from routers  import main
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(main.router)
 
 @app.get("/")
 async def create_database():
 	return {"Database": "created"}
+
+
+
+
